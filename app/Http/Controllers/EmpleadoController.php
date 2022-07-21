50b852a12e8bcbc34ce1e900036d3530649bbe19
xiomara <xiomaramarchan1981@gmail.com>
2022-07-21 15:02:17 -0400

    Creacion del modelo y el comtrolador de la tabla empleados

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -1,10 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Imports\EmpleadosImport;
+use App\Exports\EmpleadosExport;
+use Excel;
 
 use Illuminate\Http\Request;
 
 class EmpleadoController extends Controller
 {
     //
+    public function index(){
+        return view('admin.empleados.data_empleados');
+     }
+  
+     // Importar data a la base de datos
+     public function importarDataEmpleados(Request $request){
+        Excel::import(new EmpleadosImport, $request->file('file')->store('temp'));
+        return back()->with('success', 'Data importada con éxito!');
+     }
+  
+     // Exportar data de la base de datos
+    public function exportarDataEmpleados(Request $request) 
+    {        
+        return Excel::download(new EmpleadosExport, 'personal_alcaldia.xlsx');
+    }
 }
--- app/Models/Empleado.php
+++ app/Models/Empleado.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Empleado extends Model
 {
     use HasFactory;
+    protected $fillable = [
+
+        'cedula','nombres','apellidos','telefono','email','direccion'
+     ];
 }
