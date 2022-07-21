4fd3274673bdcab6a9a88af4a78338e18e055ebd
xiomara <xiomaramarchan1981@gmail.com>
2022-07-21 12:07:43 -0400

    Creacion del modelo y migracion de la tabla Nominas

---
+++ database/migrations/2022_07_21_154501_create_nominas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNominasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nominas', function (Blueprint $table) {
+            $table->id();
+            $table->descripcion();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nominas');
+    }
+}
