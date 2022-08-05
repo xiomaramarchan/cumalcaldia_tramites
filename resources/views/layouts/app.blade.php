77ee59eed619bed5b570f687eb5c12a6032ad0bc
xiomara <xiomaramarchan1981@gmail.com>
2022-08-05 12:11:15 -0400

    Agregue la fecha con carbon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,18 +27,13 @@
             <!-- Primer menu: Hora y redes sociales -->
                 <nav class="navbar navbar-expand gr-nav-2">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#">
+                        <a class="navbar-brand">      
                             @php 
                              use Carbon\Carbon;
                              $diaActual = Carbon::now()->translatedFormat('d \d\e F \d\e Y'); 
                              echo $diaActual
                              @endphp 
-                           </a><span id="HoraActual"></span> 
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <a class="navbar-brand">18 de julio de 2022 15:45</a>
+                           </a><span id="HoraActual"></span> </a>
                         <div class="" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 redes-sociales">
                             <li class="nav-item">
